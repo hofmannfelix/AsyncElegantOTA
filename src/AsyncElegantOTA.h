cc86339eeb63c0c316385bfd1107875bdd872cc4
hofmannfelix <46502301+hofmannfelix@users.noreply.github.com>
2025-10-05 21:45:42 +0200

    Update AsyncElegantOTA.h

--- src/AsyncElegantOTA.h
+++ src/AsyncElegantOTA.h
@@ -13,7 +13,7 @@
     #include "WiFi.h"
     #include "AsyncTCP.h"
     #include "Update.h"
-    #include "esp_int_wdt.h"
+    #include "esp_private/esp_int_wdt.h"
     #include "esp_task_wdt.h"
 #endif
 
